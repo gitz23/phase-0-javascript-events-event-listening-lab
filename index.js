7a6d2cf1604d681c2fc46e1e32f4c87b67513b85
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-25 23:07:55 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+const input = document.getElementById('input');
+const add = ()=> alert('I was clicked!');
+input.addEventListener('click', add);
+};
+
+addingEventListener();
